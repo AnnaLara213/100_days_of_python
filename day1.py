17228e45d2d7616fc6e4d36d739c23d5798d6cba
Anna Lara Ferreira <66583737+AnnaLara213@users.noreply.github.com>
2025-03-13 16:19:28 -0300

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,9 @@
+#Criando um nome de banda com o nome da sua cidade e do seu pet.
+
+print("Welcome to the Band Name Generator!")
+city_name = input("What's the name of the city you grew up in?\n")
+
+pet_name = input("What's your pet's name?\n")
+
+
+print("Your band name could be " + city_name + " " +pet_name + ".")
